6f6cc00d445aecb2d9feb0f1f2c28526464e19f6
Armenn25 <168114760+Armenn25@users.noreply.github.com>
2024-05-09 11:20:29 +0200

    publish v1

---
+++ java.js
@@ -0,0 +1,211 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+      
+
+/*===== MENU SHOW =====*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+       
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+      
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+   
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+    
+}
+window.addEventListener('scroll', scrollHeader)
+
+// swiper
+var mySwiper = new Swiper('.mySwiper', {
+    // Opcije
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+    
+  
+    // Automatsko prebacivanje
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+});
+
+//swipergallery
+var swiper = new Swiper(".gallery_swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
+
+  /*===============EMAIL===============*/
+function sendMail(event){
+  event.preventDefault(); // Prevent the default form submission
+
+  if (!validateForm()) { // Ako forma nije validna, prekidamo funkciju
+      return;
+  }
+  
+  var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+  };
+
+  const serviceID = "service_natib1r";
+  const templateID ="template_hje87g9";
+  emailjs.send(serviceID, templateID, params)
+  .then((res) => {
+      document.getElementById("name").value="";
+      document.getElementById("email").value=""; // Fixed typo here
+      document.getElementById("message").value="";
+      console.log(res);
+      Swal.fire({
+          title: "Thank you",
+          text: "Your message has been sent!",
+          icon: "success"
+        });
+  })
+  .catch((err) => console.log(err));
+}
+//validacija 
+function validateForm() {
+  var isValid = true;
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
+  var nameError = document.getElementById('name-error');
+  var emailError = document.getElementById('email-error');
+  var messageError = document.getElementById('message-error');
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Osnovni regex za validaciju e-maila
+
+  // Validacija imena
+  if (!nameInput.value.trim()) {
+      nameError.style.display = 'flex';
+      isValid = false;
+  } else {
+      nameError.style.display = 'none';
+  }
+
+  // Validacija e-maila
+  if (!regex.test(emailInput.value)) {
+      emailError.style.display = 'flex';
+      isValid = false;
+  } else {
+      emailError.style.display = 'none';
+  }
+
+  // Validacija poruke
+  if (!messageInput.value.trim()) {
+      messageError.style.display = 'flex';
+      isValid = false;
+  } else {
+      messageError.style.display = 'none';
+  }
+  return isValid; // Ako je isValid false, sprečava slanje forme
+}
+
+//   scrollreval
+var up={
+    easing:'ease-in-out',
+    distance:'100px',
+    duration:1000,
+    delay:250,
+    origin:'top',
+    scale:1.15
+}
+var left={
+    easing:'ease-in-out',
+    distance:'200px',
+    duration:1000,
+    delay:250,
+    origin:'left',
+    scale:1.15
+}
+var right={
+    easing:'ease-in-out',
+    distance:'100px',
+    duration:1000,
+    delay:250,
+    origin:'right',
+    scale:1.15
+}
+var bot={
+    easing:'ease-in-out',
+    distance:'100px',
+    duration:1000,
+    delay:250,
+    scale:1.15
+}
+var rightm={
+    easing:'ease-in-out',
+    distance:'100px',
+    duration:1000,
+    delay:250,
+    origin:'right',
+    scale:1.15,
+    mobile:false
+}
+ScrollReveal().reveal('.title_top',up)
+ScrollReveal().reveal('.title_bot',left)
+ScrollReveal().reveal('.slogan' ,right )
+ScrollReveal().reveal('.btn_div',bot)
+ScrollReveal().reveal('.right',rightm)
+ScrollReveal().reveal('.separator',up)
+ScrollReveal().reveal('.separator_bot',up)
+ScrollReveal().reveal('.about_title',up)
+ScrollReveal().reveal('.p1',left)
+ScrollReveal().reveal('.p2',left)
+ScrollReveal().reveal('.p3',bot)
+ScrollReveal().reveal('.mid_card',left)
+ScrollReveal().reveal('.gallery_title',up)
+ScrollReveal().reveal('.contact-us_title',up)
---
+++ lang_java.js
@@ -0,0 +1,69 @@
+// lang chg
+const bih=document.querySelector('.bih')
+const eng=document.querySelector('.eng')
+let linija=document.querySelector('.lang_line')
+bih.addEventListener('click',()=>{
+  if(bih){
+    linija.classList.add('left_back')
+    linija.classList.add('bl')
+    linija.classList.remove('right_back')
+    linija.classList.remove('br')
+    document.querySelector('.home').innerHTML="POČETNA"
+    document.querySelector('.about__us').innerHTML="O NAMA"
+    document.querySelector('.gallery_l').innerHTML="GALERIJA"
+    document.querySelector('.contact_l').innerHTML="KONTAKT"
+    document.querySelector('.home1').innerHTML="Početna"
+    document.querySelector('.about__us1').innerHTML="O nama"
+    document.querySelector('.gallery_l1').innerHTML="Galerija"
+    document.querySelector('.contact_l1').innerHTML="Kontakt"
+    document.querySelector('.title_top').innerHTML="PENJAČKI CENTAR"
+    document.querySelector('.slogan').innerHTML="Penjanje je naš jezik, a stijene su naše platno. U Drežnici svaka ruta priča svoju priču, a svaki vrh je novi početak."
+    document.querySelector('.hero_btn').innerHTML="Pridruzite se"
+    document.querySelector('.about_title').innerHTML="Naša priča"
+    document.querySelector('.p1').innerHTML="Dobrodošli u Penjački centar Vrt CIKLAMA - vaše odredište za avanture i izazove! Smješten u srcu prirode, naš centar nudi izuzetno iskustvo penjanja za sve uzraste i nivoe vještina. Osnovan sa vizijom promocije zdravog načina života i ljubavi prema aktivnostima na otvorenom, Vrt CIKLAMA je mjesto gdje možete testirati svoje granice, naučiti nove vještine i istinski uživati u uzbuđenjima penjanja."
+    document.querySelector('.p2').innerHTML="Pridružite nam se u Vrtu ciklama i doživite penjanje na potpuno nov način. Bilo da tražite novi hobi ili želite da izgradite svoje penjačke vještine, naš centar je idealno mjesto za vas. Otkrijte svoj novi omiljeni izazov - vidimo se na vrhu!"
+    document.querySelector('.p3').innerHTML="Više"
+    document.querySelector('.gallery_title').innerHTML="Galerija"
+    document.querySelector('.contact-us_title').innerHTML="Kontakt"
+    document.querySelector('.name_f').innerHTML="Ime"
+    document.querySelector('.msg').innerHTML="Poruka"
+    document.querySelector('.footer_btn').innerHTML="Pošalji"
+    document.querySelector('.adresa').innerHTML="<b>Adresa:</b>Donja Dreznica"
+    document.querySelector('.footer_title').innerHTML="PENJAČKI CENTAR VRT CIKLAMA"
+    document.querySelector('.tt1').innerHTML="NAVIGACIJA"
+    document.querySelector('.tt2').innerHTML="PROFILI"
+  }
+})
+eng.addEventListener('click',()=>{
+  if(eng){
+    linija.classList.remove('left_back')
+    linija.classList.add('br')
+    linija.classList.add('right_back')
+    linija.classList.remove('bl')
+    document.querySelector('.home').innerHTML="HOME"
+    document.querySelector('.about__us').innerHTML="ABOUT US"
+    document.querySelector('.gallery_l').innerHTML="GALLERY"
+    document.querySelector('.contact_l').innerHTML="CONTACT"
+    document.querySelector('.home1').innerHTML="Home"
+    document.querySelector('.about__us1').innerHTML="About Us"
+    document.querySelector('.gallery_l1').innerHTML="Gallery"
+    document.querySelector('.contact_l1').innerHTML="Contact"
+    document.querySelector('.title_top').innerHTML="CLIMBING CENTER"
+    document.querySelector('.slogan').innerHTML="Climbing is our language, and the rocks are our canvas. In Drežnica, every route tells its story, and every peak is a new beginning."
+    document.querySelector('.hero_btn').innerHTML="Join Us"
+    document.querySelector('.about_title').innerHTML="Our Story"
+    document.querySelector('.p1').innerHTML="Welcome to the Vrt CIKLAMA Climbing Center - your destination for adventure and challenge! Located in the heart of nature, our center offers an exceptional climbing experience for all ages and skill levels. Founded with a vision of promoting a healthy lifestyle and a love for outdoor activities, Vrt CIKLAMA is the place where you can test your limits, learn new skills, and truly enjoy the thrills of climbing."
+    document.querySelector('.p2').innerHTML="Join us in the Vrtu ciklama and experience climbing in a completely new way. Whether you are looking for a new hobby or want to build your climbing skills, our center is the ideal place for you. Discover your new favorite challenge - see you at the top!"
+    document.querySelector('.p3').innerHTML="See more"
+    document.querySelector('.gallery_title').innerHTML="Gallery"
+    document.querySelector('.contact-us_title').innerHTML="Contact Us"
+    document.querySelector('.name_f').innerHTML="Name"
+    document.querySelector('.msg').innerHTML="Send Message"
+    document.querySelector('.adresa').innerHTML="<b>Address:</b>Donja Dreznica"
+    document.querySelector('.footer_title').innerHTML="CLIMBING CENTER VRT CIKLAMA"
+    document.querySelector('.tt1').innerHTML="NAVIGATION"
+    document.querySelector('.tt2').innerHTML="SOCIAL"
+
+
+  }
+})
